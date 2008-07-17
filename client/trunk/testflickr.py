46603614ccb0b7634885d4f4e19f50df79d49e84
Ali Karbassi <ali.karbassi@gmail.com>
2008-07-17 22:27:38 +0000

    Removed checks, uploads to flickr automatically
    git-svn-id: http://yankee.bravo/svn/projects/nerdstream@17921 1d10313c-ca1c-0410-8c50-c69dad79ac7c

--- client/trunk/testflickr.py
+++ client/trunk/testflickr.py
@@ -10,34 +10,34 @@ secret_key='2e9f114458f5889e'
 flickr = FlickrAPI(api_key,secret_key, format='etree')
 
 #authenticate
-(token, frob) = flickr.get_token_part_one(perms='write')
-if not token:
-	raw_input("Press ENTER after you authorized this program")
-flickr.get_token_part_two((token, frob))
+# (token, frob) = flickr.get_token_part_one(perms='write')
+# if not token:
+# 	raw_input("Press ENTER after you authorized this program")
+# flickr.get_token_part_two((token, frob))
 
-ret = flickr.test_login(api_key=api_key)
+# # Get Login information
+# ret = flickr.test_login(api_key=api_key)
+# for i in ret:
+# 	print i.attrib['id'] == user_id
 
-for i in ret:
-	print i.attrib['id'] == user_id
+# the call back function
+def func(progress, done):
+	if done:
+		print "done"
+	else:
+		print "at %s%%" % progress
 
-# # the call back function
-# def func(progress, done):
-# 	if done:
-# 		print "done"
-# 	else:
-# 		print "at %s%%" % progress
-# 
-# #the upload function, change the filename, and tag, or if want it to be private, change is_public=1 to is_public=0
-# subprocess.call("./isightcapture -t png test.png", shell=True)
-# rsp = flickr.upload(filename="test.png",
-# 	callback=func,
-# 	title='Ali Karbassi',
-# 	description = 'Testing Upload feature.',
-# 	tags='NerdStream ns:user="Ali Karbassi" ns:title="Programmer"',
-# 	is_public='0',
-# 	is_friend='1',
-# 	is_family='0',
-# 	safety_level='1',
-# 	content_type='1',
-# 	hidden='1'
-# 	)
+#the upload function, change the filename, and tag, or if want it to be private, change is_public=1 to is_public=0
+subprocess.call("./isightcapture -t png test.png", shell=True)
+rsp = flickr.upload(filename="test.png",
+	callback=func,
+	title='Ali Karbassi',
+	description = 'Testing Upload feature.',
+	tags='NerdStream ns:user="Ali Karbassi" ns:title="Programmer"',
+	is_public='0',
+	is_friend='1',
+	is_family='0',
+	safety_level='1',
+	content_type='1',
+	hidden='1'
+	)
