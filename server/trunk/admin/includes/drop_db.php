ec3a61c715a3cdbc46ab62a6ec54a5c48c9e2311
Ali Karbassi <ali.karbassi@gmail.com>
2008-07-20 05:07:41 +0000

    Deleting old server. Committing new AppEngine version.
    git-svn-id: http://yankee.bravo/svn/projects/nerdstream@17981 1d10313c-ca1c-0410-8c50-c69dad79ac7c

--- server/trunk/admin/includes/drop_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-execute('drop database', array('db' => $_GET['db']));
-header('Location: index.php?page=view_databases');
-
-?>
--- server/trunk/admin/includes/empty_table.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-execute('delete', array(
-	'db'    => $_GET['db'],
-	'table' => $_GET['table']
-));
-
-header('Location: index.php?page=table_prop&db='.$_GET['db'].'&table='.$_GET['table']);
-?>
