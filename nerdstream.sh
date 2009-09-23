2146086e88bb541e6f71bf3de88f3c2c1664890e
Ali Karbassi <ali@karbassi.com>
2009-09-23 17:14:01 -0500

    New version of nerdstream using only technology that macs have built in. No more python.

---
+++ nerdstream.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get username
+name=`whoami`
+
+# Set capture folder
+dir=`pwd`'/capture/'
+
+# Create capture folder if it doesn't exist
+if [[ ! -d "$dir" ]]; then
+    mkdir -p $dir
+fi
+
+# Set file name
+file=$dir"`date +%Y-%m-%d_%H-%M-%S`.jpg"
+
+# Take the picture
+`pwd`'/isightcapture' $file
+
+# Upload file
+curl -F "img=@$file;type=image/jpeg" -F "name=$name" http://ali.karbassi.com/isight/test.php
