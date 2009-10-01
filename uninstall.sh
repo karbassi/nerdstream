d42413266274dd7e4140bf7c681300ec7bf51672
Ali Karbassi <ali@karbassi.com>
2009-10-01 10:06:02 -0500

    Changed the default permissions because the install.sh will handle it.
    uninstall.sh will now make sure the launchctl is removed from the list.

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,8 @@
-file=~/Library/LaunchAgents/ali.karbassi.nerdstream.plist
+name=ali.karbassi.nerdstream.plist
+file=~/Library/LaunchAgents/$name
 sudo launchctl stop $file
 sudo launchctl unload $file
+sudo launchctl remove $name
 sudo rm $file
 sudo rm -rf .capture
 sudo rm capture.sh
